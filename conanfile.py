413d79b5eb0eef088893edbe8655d0ea385eec28
Jerry W <jerrywiltse@gmail.com>
2017-08-06 20:48:02 -0400

    changed to header only per rene

--- conanfile.py
+++ conanfile.py
@@ -3,18 +3,17 @@ from conans import ConanFile, tools, os
 class BoostMetaparseConan(ConanFile):
     name = "Boost.Metaparse"
     version = "1.64.0"
-    generators = "txt" 
-    settings = "os", "arch", "compiler", "build_type"
+    generators = "boost" 
     url = "https://github.com/boostorg/metaparse"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "metaparse"
-    build_requires = "Boost.Build/1.64.0@bincrafters/testing" 
     requires =  "Boost.Config/1.64.0@bincrafters/testing", \
                       "Boost.Mpl/1.64.0@bincrafters/testing", \
                       "Boost.Predef/1.64.0@bincrafters/testing", \
                       "Boost.Preprocessor/1.64.0@bincrafters/testing", \
                       "Boost.Static_Assert/1.64.0@bincrafters/testing", \
+                      "Boost.Test/1.64.0@bincrafters/testing", \
                       "Boost.Type_Traits/1.64.0@bincrafters/testing"
 
                       #config0 mpl5 predef0 preprocessor0 static_assert1 type_traits3
@@ -27,3 +26,5 @@ class BoostMetaparseConan(ConanFile):
         include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
         self.copy(pattern="*", dst="include", src=include_dir)
 
+    def package_id(self):
+        self.info.header_only()
