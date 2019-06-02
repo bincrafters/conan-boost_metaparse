6f42126a05a6931950c5b69b023b0ccfc06fdcbc
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:06 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,19 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostMetaparseConan(base.BoostBaseConan):
     name = "boost_metaparse"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_metaparse"
-    lib_short_names = ["metaparse"]
-    header_only_libs = ["metaparse"]
-    b2_requires = [
-        "boost_config",
-        "boost_mpl",
-        "boost_predef",
-        "boost_preprocessor",
-        "boost_static_assert",
-        "boost_type_traits"
-    ]
+    version = "1.70.0"
